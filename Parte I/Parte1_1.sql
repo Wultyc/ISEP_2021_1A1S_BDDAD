2f369127ccb30ef2c5a2bb1aef756c7fe0264cb0
DiogoMoreteBarreto <1150468@isep.ipp.pt>
2020-11-21 14:25:08 +0000

    Correcao do ex 1 a) e implementacao incompleta do ex 1 b)

--- Parte I/Parte1_1.sql
+++ Parte I/Parte1_1.sql
@@ -1,5 +1,4 @@
 /*Ex 1 a)*/
 
-
 (select nrFuncionario from IntervencaoQuarto iq,Manutencao man where iq.id = man.intervencaoQuartoId 
- and (sysdate-2) < iq.dataIntervencao and iq.concluido ='Y');
+ and sysdate > iq.dataIntervencao and iq.concluido = 'N');
