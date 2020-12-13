3743f5f923a28ad714c1ef72b99d6f1a6f6427a4
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-13 19:33:17 +0000

    Algumas melhorias

--- Parte II/alunoB-4.sql
+++ Parte II/alunoB-4.sql
@@ -7,21 +7,7 @@ IS
     v_mes INT;
     v_ano INT;
     
-    CURSOR c_total_consumos_reg_camareira(cp_mes INT, cp_ano INT) IS
-        SELECT
-            funcionario.id,
-            funcionario.nome,
-            sum(artigo_consumo.preco) as "total_consumos_registados",
-            min(linha_conta_consumo.data_registo) as "primeiro_registo",
-            max(linha_conta_consumo.data_registo) as "ultimo_registo"
-        FROM camareira
-            JOIN funcionario ON funcionario.id = camareira.id
-            JOIN linha_conta_consumo ON camareira.id = linha_conta_consumo.id_camareira
-            JOIN artigo_consumo ON linha_conta_consumo.id_artigo_consumo = artigo_consumo.id
-        WHERE
-            extract(year from linha_conta_consumo.DATA_REGISTO) = cp_ano AND
-            extract(month from linha_conta_consumo.DATA_REGISTO) = cp_mes
-        GROUP BY funcionario.id, funcionario.nome;
+    c_total_consumos_reg_camareira SYS_REFCURSOR;        
     
     mes_invalido EXCEPTION;
 
@@ -39,7 +25,20 @@ BEGIN
         v_ano := p_ano;
     END IF;
     
-    OPEN c_total_consumos_reg_camareira(v_mes, v_ano);
+    OPEN c_total_consumos_reg_camareira FOR
+        SELECT
+            funcionario.id,
+            funcionario.nome,
+            sum(linha_conta_consumo.preco_unitario*linha_conta_consumo.quantidade) as "total_consumos_registados",
+            min(linha_conta_consumo.data_registo) as "primeiro_registo",
+            max(linha_conta_consumo.data_registo) as "ultimo_registo"
+        FROM camareira
+            JOIN funcionario ON funcionario.id = camareira.id
+            JOIN linha_conta_consumo ON camareira.id = linha_conta_consumo.id_camareira
+        WHERE
+            extract(year from linha_conta_consumo.DATA_REGISTO) = v_ano AND
+            extract(month from linha_conta_consumo.DATA_REGISTO) = p_mes
+        GROUP BY funcionario.id, funcionario.nome;
     
     return c_total_consumos_reg_camareira;
     
@@ -50,11 +49,28 @@ EXCEPTION
 END;
 
 /
-
 BEGIN
-    dbms_output.put_line(FNCOBTERREGISTOMENSALCAMAREIRA(9));
-    dbms_output.put_line('=====================================');
-    dbms_output.put_line(FNCOBTERREGISTOMENSALCAMAREIRA(9,2020));
-    dbms_output.put_line('=====================================');
-    dbms_output.put_line(FNCOBTERREGISTOMENSALCAMAREIRA(91));
+    DECLARE
+        cur1 SYS_REFCURSOR;
+    BEGIN
+    --Mes: 9 | Ano: default
+    cur1 := fncObterRegistoMensalCamareira(9);
+    DBMS_SQL.RETURN_RESULT(cur1);
+    END;
+    
+    DECLARE
+        cur2 SYS_REFCURSOR;
+    BEGIN
+        --Mes: 10 | Ano: 2020
+        cur2 := fncObterRegistoMensalCamareira(10,2020);
+        DBMS_SQL.RETURN_RESULT(cur2);
+    END;
+    
+    DECLARE
+        cur3 SYS_REFCURSOR;
+    BEGIN
+        --Mes: 92020 | Ano: default - simulação de erro
+        cur3 := fncObterRegistoMensalCamareira(92020);
+        DBMS_SQL.RETURN_RESULT(cur3);
+    END;
 END;
