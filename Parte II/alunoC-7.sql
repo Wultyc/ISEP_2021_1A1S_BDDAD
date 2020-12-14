733e5278cc7b7f4f3a9f42f46d7e817c8d6ffcfa
Diogo Barreto <diogo.barreto@farfetch.com>
2020-12-14 12:01:16 +0000

    Correction of ex7 part 2

--- Parte II/alunoC-7.sql
+++ Parte II/alunoC-7.sql
@@ -2,16 +2,18 @@ create or replace function fncDisponibilidadeReserva(p_id_quarto int, p_data dat
 is
 available Boolean:= true;
 numDias Number := 0;
+new_date DATE := SYSDATE;
 BEGIN
     -- is available?
 
 WHILE numDias <= p_duracao
 LOOP
 if not isQuartoIndisponivel(p_id_quarto, p_data) then
-   SELECT TO_DATE(p_data) + 1 FROM DUAL;
+   SELECT (TO_DATE(p_data) + 1) into new_date FROM DUAL;
    numDias := numDias +1;
    ELSE
       available := False;
+      END IF;
 END LOOP;
     RETURN available;
 END;
