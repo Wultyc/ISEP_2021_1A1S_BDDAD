602980cf60a98288e43bbae065e24ef9e0a6007a
Diogo Barreto <diogo.barreto@farfetch.com>
2020-12-17 03:49:37 +0000

    Ex 7 part 2 implemented

--- Parte II/alunoC-7.sql
+++ Parte II/alunoC-7.sql
@@ -1,3 +1,4 @@
+
 create or replace function fncDisponibilidadeReserva(p_id_quarto int, p_data date, p_duracao int, p_num_pessoas int) return boolean
 is
 available Boolean:= true;
@@ -5,15 +6,31 @@ numDias Number := 0;
 new_date DATE := SYSDATE;
 BEGIN
     -- is available?
-
+SELECT (TO_DATE(p_data)) into new_date FROM DUAL;
 WHILE numDias <= p_duracao
 LOOP
-if not isQuartoIndisponivel(p_id_quarto, p_data) then
-   SELECT (TO_DATE(p_data) + 1) into new_date FROM DUAL;
-   numDias := numDias +1;
+available :=isQuartoIndisponivel(p_id_quarto, new_date);
+if available = false then
+EXIT;   
    ELSE
-      available := False;
+   dbms_output.put_line('Chegou');
+   new_date := new_date +1;
+   numDias := numDias+1;
+    
       END IF;
+      
 END LOOP;
+if available = TRUE then
+    dbms_output.put_line('TRUE');
+  else
+    dbms_output.put_line('FALSE');
+  end if;
     RETURN available;
 END;
+
+
+declare 
+resultado boolean;
+begin
+    resultado := fncDisponibilidadeReserva(1, to_date('11/01/2020', 'dd/mm/yyyy'), 1, 1);
+end;
